67fe01896160bbf82877229d2d4c629c02e7c019
kyp <kyp@kmc.gr.jp>
2021-01-20 00:21:05 +0900

    ヌイート下部のボタンをリライト

--- app/frontend/src/nweets.ts
+++ app/frontend/src/nweets.ts
@@ -1,4 +1,4 @@
-let setIndividualLikeButton = (event:Event) => {
+let setIndividualLikeButton = (event: Event) => {
   let div = <HTMLElement>event.currentTarget;
 
   let anchor = <HTMLElement>div.firstElementChild;
@@ -8,21 +8,21 @@ let setIndividualLikeButton = (event:Event) => {
   // let likeNumElement = <HTMLElement>anchor.childNodes[1];
   anchor.classList.toggle('liked');
 
-  if(icon.classList.contains('fas')){
-    icon.classList.replace('fas', 'far');
+  if (icon.classList.contains('bi-heart-fill')) {
+    icon.classList.replace('bi-heart-fill', 'bi-heart');
     anchor.setAttribute('data-method', 'post');
     outerLatestLikedTime.classList.remove('d-none');
     likeFlash.innerText = '';
-  }else{
-    icon.classList.replace('far', 'fas');
+  } else {
+    icon.classList.replace('bi-heart', 'bi-heart-fill');
     anchor.setAttribute('data-method', 'delete');
     likeFlash.innerText = "「いいね」しました！";
     outerLatestLikedTime.classList.add('d-none');
   }
 }
 
-export function setLikeButtons(){
-  document.querySelectorAll('.like-btn').forEach(function(div){
+export function setLikeButtons() {
+  document.querySelectorAll('.like-btn-wrapper').forEach(function (div) {
     div.addEventListener('ajax:success', setIndividualLikeButton);
   });
 };
